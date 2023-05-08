71ceaa44a7c1a9e1591aa4c2fac909da9588b55b
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-08 15:39:37 -0400

    added images

--- getMyFace.py
+++ getMyFace.py
@@ -5,3 +5,16 @@ import numpy as np
 #This project uses our webcam and tries to detect and, if possible, recognize the faces in view
 
 #To speed things up we will use 1/4 the resolution of the camera and skip every other frame 
+
+video_capture = cv2.VideoCapture(0) #references webcam 0
+
+#Load in our first sample face
+todd_image = face_recognition.load_image_file("todd.jpeg")
+todd_face_encoding = face_recognition.face_encodings(todd_image)[0]
+
+#Do the same for the second image
+damian_image = face_recognition.load_image_file("damian.jpg")
+damian_face_encoding = face_recognition.face_encodings(damian_image)[1]
+
+jiho_image = face_recognition.load_image_file("jiho.png")
+jiho_face_encoding = face_recognition.face_encodings(jiho_image)[2]
