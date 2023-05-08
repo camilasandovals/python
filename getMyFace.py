a6046cba5a528915eb5dc583e0dae31e62912888
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-08 16:16:40 -0400

    added rectangles

--- getMyFace.py
+++ getMyFace.py
@@ -18,3 +18,56 @@ damian_face_encoding = face_recognition.face_encodings(damian_image)[1]
 
 jiho_image = face_recognition.load_image_file("jiho.png")
 jiho_face_encoding = face_recognition.face_encodings(jiho_image)[2]
+
+#create an array of know face encodings and their names
+known_face_encodings = [todd_face_encoding, damian_face_encoding, jiho_face_encoding]
+known_face_name = ["Todd A." , "Damian M.", "Jiho S."]
+
+#Set up some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True: 
+    ret, frame = video_capture.read()
+
+    if process_this_frame:
+        #resize frame to 1/4 size for speed 
+        small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
+        #convert the image from BGR (which opencv uses) to RGC which face_recognition uses
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        #Find all of the faces in the current frame
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face in face_encodings:
+            #See if this face matches our known faces
+            matches = face_recognition.compare_faces(known_face_encodings, face)
+            name = "Unknown"
+            #We will use the known_face with the smallest distance to the current face
+            face_distances = face_recognition.face_distance(known_face_encodings, face)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_name[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+    #Display the results
+    for(top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+    
+    #Draw a red box around each face
+    cv2.rectangle(frame, (left, top), (right, bottom), (0,0, 255), 2)
+
+    #Draw a label with the name below the face
+    cv2.rectangle(frame, (left, bottom -35), (right, bottom), (0,0,255), cv2.FILLED)
+    
+
